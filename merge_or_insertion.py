25dfc210fac3773c7bbb8e3bacb7daa8d98fd861
Matt C <mcmpbell@uab.edu>
2022-12-13 18:17:25 -0600

    Update merge_or_insertion.py

--- merge_or_insertion.py
+++ merge_or_insertion.py
@@ -1,75 +1,43 @@
 import timeit
 
 with open("input_100.txt", "r") as ri:
-    
         temp = ri.readline()
-
         forSort = list(map(int, temp.split()))
-
         start = timeit.default_timer()
 
 def merge_sort(arr):
-    
     if len(arr) > 1:
-
         a = b = c = 0
-        
         split = (len(arr) // 2)
-        
         right = arr[:split]
-        
         left = arr[split:]
-
         merge_sort(left)
-        
         merge_sort(right)
-        
         while a < len(left) and b < len(right):
-            
             if left[a] < right[b]:
-                
                 arr[c] = left[a]
-                
                 a += 1
-                
             else:
-                
                 arr[c] = right[b]
-                
                 b += 1
-                
             c += 1
-
         while a < len(left):
-            
             arr[c] = left[a]
-            
             a += 1
-            
             c += 1
-
         while b < len(right):
-            
             arr[c] = right[b]
-            
             b += 1
-            
             c += 1
             
 def insertion_sort(arr):
         
     for i in range(0, len(arr)):
-
         cval = arr[i]
-        
         pos = i
-
         while pos > 0 and arr[pos - 1] > cval:
-            
             arr[pos] = arr[pos - 1]
-            
             pos = pos - 1
-
         arr[pos] = cval
     
 arr = forSort
