25b39fa4fa999a7c177fff98820f01fe9ec230d5
Matt C <mcmpbell@uab.edu>
2022-12-13 18:13:19 -0600

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -1,18 +1,10 @@
 def binarySearch(array, key, start, end):    
-    
     if end is None:
-        
         end = len(array) - 1              
-
     middle = (start + end) // 2           
-    
     if key == array[middle]:             
-        
         return middle
-    
     if key < array[middle]:              
-        
         return (binarySearch(array, key, start, middle - 1))
-    
     return (binarySearch(array, key, middle + 1, end)) 
 
