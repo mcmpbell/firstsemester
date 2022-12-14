1c68f3007a390c6ae8d98e7db60a6001eb041395
Matt C <mcmpbell@uab.edu>
2022-12-13 18:18:47 -0600

    Update merge_sort.py

--- merge_sort.py
+++ merge_sort.py
@@ -1,48 +1,26 @@
 def merge_sort(arr):
-    
     if len(arr) > 1:
-
         a = b = c = 0
-
         split = len(arr) // 2
-        
         left = arr[:split]                     
-        
         right = arr[split:]                    
-
         merge_sort(left)                            
-        
         merge_sort(right)                           
-        
         while a < len(left) and b < len(right):             
-            
             if left[a] < right[b]:                      
-                
                 arr[c] = left[a]                        
-                
                 a += 1
             else:
-                
                 arr[c] = right[b]                       
-                
                 b += 1
-                
             c += 1
-
         while a < len(left):
-            
             arr[c] = left[a]
-            
             a += 1
-            
             c += 1
-
         while b < len(right):
-            
             arr[c] = right[b]
-            
             b += 1
-            
             c += 1
 
 
