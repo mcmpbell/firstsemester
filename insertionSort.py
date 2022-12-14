04020e11185866924d4767a40956fc360d934ff7
Matt C <mcmpbell@uab.edu>
2022-12-13 18:14:25 -0600

    Update insertionSort.py

--- insertionSort.py
+++ insertionSort.py
@@ -1,17 +1,10 @@
 def insertionSort(Sort):
-    
     for i in range(1, len(Sort)):
-
         cval = Sort[i]
-        
         pos = i
-
         while pos > 0 and Sort[pos - 1] > cval:
-            
             Sort[pos] = Sort[pos - 1]
-            
             pos = pos - 1
-
         Sort[pos] = cval
 
 
